9b626e382c9c60b12b085808775b5928cb34a52c
Challa <csharath@outlook.com>
2019-09-22 12:21:01 +0530

    create develop branch

--- server/app.js
+++ server/app.js
@@ -27,12 +27,12 @@ app.use("/users", usersRouter);
 app.use("/testAPI", testAPIRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
     next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
     // set locals, only providing error in development
     res.locals.message = err.message;
     res.locals.error = req.app.get("env") === "development" ? err : {};
