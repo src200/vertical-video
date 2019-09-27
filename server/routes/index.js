378b521717b17323f1f9510aebc955d3df87c933
Challa <csharath@outlook.com>
2019-09-27 23:03:36 +0530

    feat(upload): file upload api

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
+var formidable = require('formidable');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.post('/upload', function (req, res){
+  var form = new formidable.IncomingForm();
+
+  form.parse(req);
+
+  form.on('fileBegin', function (name, file){
+      file.path = __dirname + '/uploads/' + file.name;
+  });
+
+  form.on('file', function (name, file){
+      console.log('Uploaded ' + file.name);
+  });
+
+  form.on('progress', function (bytesReceived, bytesExpected) {
+    var percentComplete = (bytesReceived / bytesExpected) * 100;
+    console.log(percentComplete.toFixed(2));
+  });
+
+  form.on('end', function () {
+    res.send('File uploaded!');
+  });
+
+  form.on('aborted', function () {
+    res.send('File Upload aborted');
+  });
+
+  form.on('error', function (err) {
+    res.send('Error occured while uploading file. Please try again.');
+  });
 });
 
 module.exports = router;
