4c12a16b3cee6e5d4790970fa9145b1105513eb3
Challa Sharath <csharath@lantronix.com>
2019-11-15 13:16:54 +0530

    feat(opencv): use openCV to calcuate optical flow

--- client/src/components/player/Player.js
+++ client/src/components/player/Player.js
@@ -4,6 +4,7 @@ import '../resizer/Resizer.scss'
 // import Resizer from '../resizer/Resizer';
 import { Slider, Button } from 'antd';
 import { Rnd } from 'react-rnd';
+const cv = window.cv;
 
 class Player extends Component {
     constructor(props) {
@@ -106,6 +107,72 @@ class Player extends Component {
         //     }
         // });
 
+        const startOpticFlow = () => {
+            let video = this.videoEl.current;
+            let cap = new cv.VideoCapture(video);
+
+            // take first frame of the video
+            let frame1 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+            cap.read(frame1);
+
+            let prvs = new cv.Mat();
+            cv.cvtColor(frame1, prvs, cv.COLOR_RGBA2GRAY);
+            frame1.delete();
+            let hsv = new cv.Mat();
+            let hsv0 = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+            let hsv1 = new cv.Mat(video.height, video.width, cv.CV_8UC1, new cv.Scalar(255));
+            let hsv2 = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+            let hsvVec = new cv.MatVector();
+            hsvVec.push_back(hsv0); hsvVec.push_back(hsv1); hsvVec.push_back(hsv2);
+
+            let frame2 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+            let next = new cv.Mat(video.height, video.width, cv.CV_8UC1);
+            let flow = new cv.Mat(video.height, video.width, cv.CV_32FC2);
+            let flowVec = new cv.MatVector();
+            let mag = new cv.Mat(video.height, video.width, cv.CV_32FC1);
+            let ang = new cv.Mat(video.height, video.width, cv.CV_32FC1);
+            let rgb = new cv.Mat(video.height, video.width, cv.CV_8UC3);
+
+            const FPS = 30;
+            function processVideo() {
+                try {
+                    // if (!streaming) {
+                    //     // clean and stop.
+                    //     prvs.delete(); hsv.delete(); hsv0.delete(); hsv1.delete(); hsv2.delete();
+                    //     hsvVec.delete(); frame2.delete(); flow.delete(); flowVec.delete(); next.delete();
+                    //     mag.delete(); ang.delete(); rgb.delete();
+                    //     return;
+                    // }
+                    let begin = Date.now();
+
+                    // start processing.
+                    cap.read(frame2);
+                    cv.cvtColor(frame2, next, cv.COLOR_RGBA2GRAY);
+                    cv.calcOpticalFlowFarneback(prvs, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
+                    cv.split(flow, flowVec);
+                    let u = flowVec.get(0);
+                    let v = flowVec.get(1);
+                    cv.cartToPolar(u, v, mag, ang);
+                    u.delete(); v.delete();
+                    ang.convertTo(hsv0, cv.CV_8UC1, 180 / Math.PI / 2);
+                    cv.normalize(mag, hsv2, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1);
+                    cv.merge(hsvVec, hsv);
+                    cv.cvtColor(hsv, rgb, cv.COLOR_HSV2RGB);
+                    cv.imshow('canvasOutput', rgb);
+                    next.copyTo(prvs);
+
+                    // schedule the next one.
+                    let delay = 1000 / FPS - (Date.now() - begin);
+                    setTimeout(processVideo, delay);
+                } catch (err) {
+                    console.error(err);
+                }
+            };
+
+            // schedule the first one.
+            window.setTimeout(processVideo, 0);
+        }
+
         const drawFrames = (videoDOM) => {
             if (!videoDOM.paused && !videoDOM.ended) {
                 ctx.drawImage(videoDOM, 0, 0, ctx.canvas.width, ctx.canvas.height);
@@ -136,6 +203,7 @@ class Player extends Component {
         // event triggered on playing video
         this.videoEl.current.addEventListener('play', (e) => {
             drawFrames(this.videoEl.current);
+            startOpticFlow();
         });
 
         // event triggered while playing video
@@ -167,7 +235,7 @@ class Player extends Component {
     render() {
         return (
             <div className="player">
-                <video src={this.props.videoSrc} ref={this.videoEl} style={{ display: 'none' }}>
+                <video width="500" height="500" src={this.props.videoSrc} ref={this.videoEl} style={{ display: 'none' }}>
                     Sorry, your browser doesn't support embedded videos.
                 </video>
                 <div className="canvas-container">
@@ -183,7 +251,9 @@ class Player extends Component {
                 </div>
                 <div className="preview-container">
                     <canvas ref={this.previewCanvasEl} width={this.state.previewFrameGeometry.sWidth} height={this.state.previewFrameGeometry.sHeight}></canvas>
+                    <canvas id="canvasOutput"></canvas>
                 </div>
+
             </div>
         )
     }
