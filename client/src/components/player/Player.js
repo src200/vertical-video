dfaa1e4a43a3d052dac74a4ee7d7373425951263
Challa Sharath <csharath@lantronix.com>
2019-11-21 15:55:43 +0530

    fix(video-process): separate video processing routine

--- client/src/components/player/Player.js
+++ client/src/components/player/Player.js
@@ -83,6 +83,7 @@ class Player extends Component {
 
     play() {
         this.videoEl.current.play();
+        this.initVideoProcessing();
     }
 
     pause() {
@@ -93,85 +94,45 @@ class Player extends Component {
         this.videoEl.current.currentTime = seekTo;
     }
 
+    initVideoProcessing() {
+        let video = this.videoEl.current;
+        let cap = new cv.VideoCapture(video);
+
+        
+        let src = new cv.Mat();
+        let dst = new cv.Mat();
+        const FPS = 30;
+        const processVideo = () => {
+            try {
+                if (video.paused) {
+                    // clean and stop.
+                    src.delete(); dst.delete();
+                    return;
+                }
+                let begin = Date.now();
+
+                cap.read(src);
+                cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+                cv.Laplacian(src, dst, cv.CV_8U, 1, 1, 0, cv.BORDER_DEFAULT);
+                cv.imshow("canvasOutput", dst);
+
+                // schedule the next one.
+                let delay = 1000 / FPS - (Date.now() - begin);
+                setTimeout(processVideo, delay);
+            } catch (err) {
+                console.error(err);
+            }
+        };
+
+        // schedule the first one.
+        window.setTimeout(processVideo, 0);
+    }
+
     componentDidMount() {
         const ctx = this.canvasEl.current.getContext('2d');
         const previewCtx = this.previewCanvasEl.current.getContext('2d');
         ctx.imageSmoothingEnabled = true;
         let imageData;
-
-        // // update co-ordinates of canvas element realtive to viewport
-        // this.setState({
-        //     canvasCoordinates: {
-        //         x: this.canvasEl.current.getBoundingClientRect().left,
-        //         y: this.canvasEl.current.getBoundingClientRect().top
-        //     }
-        // });
-
-        const startOpticFlow = () => {
-            let video = this.videoEl.current;
-            let cap = new cv.VideoCapture(video);
-
-            // take first frame of the video
-            let frame1 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-            cap.read(frame1);
-
-            let prvs = new cv.Mat();
-            cv.cvtColor(frame1, prvs, cv.COLOR_RGBA2GRAY);
-            frame1.delete();
-            let hsv = new cv.Mat();
-            let hsv0 = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-            let hsv1 = new cv.Mat(video.height, video.width, cv.CV_8UC1, new cv.Scalar(255));
-            let hsv2 = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-            let hsvVec = new cv.MatVector();
-            hsvVec.push_back(hsv0); hsvVec.push_back(hsv1); hsvVec.push_back(hsv2);
-
-            let frame2 = new cv.Mat(video.height, video.width, cv.CV_8UC4);
-            let next = new cv.Mat(video.height, video.width, cv.CV_8UC1);
-            let flow = new cv.Mat(video.height, video.width, cv.CV_32FC2);
-            let flowVec = new cv.MatVector();
-            let mag = new cv.Mat(video.height, video.width, cv.CV_32FC1);
-            let ang = new cv.Mat(video.height, video.width, cv.CV_32FC1);
-            let rgb = new cv.Mat(video.height, video.width, cv.CV_8UC3);
-
-            const FPS = 30;
-            function processVideo() {
-                try {
-                    // if (!streaming) {
-                    //     // clean and stop.
-                    //     prvs.delete(); hsv.delete(); hsv0.delete(); hsv1.delete(); hsv2.delete();
-                    //     hsvVec.delete(); frame2.delete(); flow.delete(); flowVec.delete(); next.delete();
-                    //     mag.delete(); ang.delete(); rgb.delete();
-                    //     return;
-                    // }
-                    let begin = Date.now();
-
-                    // start processing.
-                    cap.read(frame2);
-                    cv.cvtColor(frame2, next, cv.COLOR_RGBA2GRAY);
-                    cv.calcOpticalFlowFarneback(prvs, next, flow, 0.5, 3, 15, 3, 5, 1.2, 0);
-                    cv.split(flow, flowVec);
-                    let u = flowVec.get(0);
-                    let v = flowVec.get(1);
-                    cv.cartToPolar(u, v, mag, ang);
-                    u.delete(); v.delete();
-                    ang.convertTo(hsv0, cv.CV_8UC1, 180 / Math.PI / 2);
-                    cv.normalize(mag, hsv2, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1);
-                    cv.merge(hsvVec, hsv);
-                    cv.cvtColor(hsv, rgb, cv.COLOR_HSV2RGB);
-                    cv.imshow('canvasOutput', rgb);
-                    next.copyTo(prvs);
-
-                    // schedule the next one.
-                    let delay = 1000 / FPS - (Date.now() - begin);
-                    setTimeout(processVideo, delay);
-                } catch (err) {
-                    console.error(err);
-                }
-            };
-
-            // schedule the first one.
-            window.setTimeout(processVideo, 0);
-        }
 
         const drawFrames = (videoDOM) => {
             if (!videoDOM.paused && !videoDOM.ended) {
@@ -189,21 +150,9 @@ class Player extends Component {
             previewCtx.putImageData(imageData, 0, 0);
         }
 
-        // {x,y} can be position object or size object 
-        const updateResizerDimensions = (geometry, x, y) => {
-            if (geometry === 'position') {
-                this.rnd.updatePosition({ x: parseInt(x), y: parseInt(y) });
-            }
-
-            if (geometry === 'size') {
-                this.rnd.updateSize({ width: x, height: y });
-            }
-        }
-
         // event triggered on playing video
         this.videoEl.current.addEventListener('play', (e) => {
             drawFrames(this.videoEl.current);
-            startOpticFlow();
         });
 
         // event triggered while playing video
@@ -235,7 +184,7 @@ class Player extends Component {
     render() {
         return (
             <div className="player">
-                <video width="500" height="500" src={this.props.videoSrc} ref={this.videoEl} style={{ display: 'none' }}>
+                <video width="500" height="400" src={this.props.videoSrc} ref={this.videoEl} style={{ display: 'none' }}>
                     Sorry, your browser doesn't support embedded videos.
                 </video>
                 <div className="canvas-container">
