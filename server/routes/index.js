9971d281686ca42a1bb735675613cb1834d5291c
Challa <csharath@outlook.com>
2020-02-22 06:19:10 +0530

    feat(opencv): video process on server

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var formidable = require('formidable');
+var cv = require('opencv4nodejs');
+var KalmanFilter = require('kalmanjs');
+ 
+const { grabFrames } = require('../utils');
 var router = express.Router();
 
 router.post('/upload', function (req, res) {
@@ -13,6 +17,44 @@ router.post('/upload', function (req, res) {
 
   form.on('file', function (name, file) {
     console.log('Uploaded ' + file.name);
+
+    // start anlaysing video
+    function initVideoProcessing() {
+      // parameters for ShiTomasi corner detection
+      let [maxCorners, qualityLevel, minDistance, blockSize] = [1000, 0.001, 3, 3];
+      // take first frame and find corners in it
+      // let srcFrame = new cv.Mat(video.height, video.width, cv.CV_8UC4);
+
+      let corners = new cv.Mat();
+      let grayFrame = new cv.Mat();
+
+      let sum, avgX;
+      const FPS = 24;
+      const kf = new KalmanFilter({ R: 0.01, Q: 4 });
+      let delay = 1000 / FPS;
+      // var out = new cv.VideoWriter('out/output.mp4', cv.VideoWriter.fourcc('MJPG'), FPS, new cv.Size(270, 480));
+
+      grabFrames(file.path, delay, (frame) => {
+        grayFrame = frame.cvtColor(cv.COLOR_RGBA2GRAY)
+        // cv.cvtColor(frame, grayFrame, cv.COLOR_RGBA2GRAY);
+        corners = cv.goodFeaturesToTrack(grayFrame, maxCorners, qualityLevel, minDistance, new cv.Mat(), blockSize);
+
+        sum = 0;
+        for (var i = 0; i < corners.length; i++) {
+          // point = new cv.Point(corners.data32F[i * 2], corners.data32F[(i * 2) + 1]);
+          // goodFeatures.push(point);
+          sum = sum + (corners[i].x - 200);
+        }
+
+        avgX = sum / corners.length;
+
+        // console.log(kf.filter(avgX));
+        // const cropRegion = frame.getRegion(new cv.Rect(50, 50, 25, 25));
+        // out.write(grayFrame);
+      });
+    }
+
+    initVideoProcessing();
   });
 
   form.on('progress', function (bytesReceived, bytesExpected) {
---
+++ server/utils.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const cv = require('opencv4nodejs');
+
+const grabFrames = (videoFile, delay, onFrame) => {
+  const cap = new cv.VideoCapture(videoFile);
+  let done = false;
+  const intvl = setInterval(() => {
+    let frame = cap.read();
+    // loop back to start on end of stream reached
+    if (frame.empty) {
+      cap.reset();
+      frame = cap.read();
+    }
+    onFrame(frame);
+
+    const key = cv.waitKey(delay);
+    done = key !== -1 && key !== 255;
+    if (done) {
+      clearInterval(intvl);
+      console.log('Key pressed, exiting.');
+    }
+  }, 0);
+};
+
+exports.grabFrames = grabFrames;
